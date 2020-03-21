76fc9723c10e6696bab24b704532948ca064ae45
George Chen <georgechen1338@gmail.com>
2020-03-21 02:43:39 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -32,6 +32,7 @@ def directors_totals(nds)
       dir_total += dir_movies[movies_index][:worldwide_gross]
       movies_index += 1
     end
+    dir_name = nds[directors_index][:name]
     directors_index += 1
   end
   #nil
