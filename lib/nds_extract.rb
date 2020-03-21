34fcedfa24d0e1978761ec41205dc4e59143f30a
George Chen <georgechen1338@gmail.com>
2020-03-21 01:55:17 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,7 +7,9 @@ def directors_totals(nds)
   #
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {}
+  result = {
+    
+  }
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
