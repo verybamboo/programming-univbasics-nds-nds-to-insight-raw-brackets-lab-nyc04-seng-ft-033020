2edf021b4e193b3c25e593a925418e3928bdad79
George Chen <georgechen1338@gmail.com>
2020-03-21 02:28:37 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,7 +9,7 @@ def directors_totals(nds)
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   
   result = {}
-  result[:name] = 
+  result[:name] = dir_total
   
   nds[directors_index][:name] 
   
