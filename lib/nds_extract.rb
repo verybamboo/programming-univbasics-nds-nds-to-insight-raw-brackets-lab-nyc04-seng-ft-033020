f8a90ef5f0d72de8c1e25f5ed1d0ffa2daa7d222
George Chen <georgechen1338@gmail.com>
2020-03-21 01:13:52 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -35,7 +35,7 @@ def directors_totals(nds)
   directors_index = 0
   while directors_index < nds.length do
     p nds[directors_index]
-    currd = nds[directors_index]
+    curr_director = nds[directors_index]
     directors_index += 1
   end
   #nil
